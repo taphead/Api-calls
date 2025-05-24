bdfe1a728e3e8cd5bd3cb9e7098de531298a4868
taphead <techne.trax@proton.me>
2025-05-24 16:18:10 +0600

    Added responsive design according to screen size

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ async function fetchUsers(url) {
   const response = await fetch(url);
   const data = await response.json();
   userArray = [...data];
-  console.log(userArray);
 }
 
 async function initializeDataDisplay() {
