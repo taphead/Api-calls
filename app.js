4b02a41a17f2dbec8103d6781327a29c0babf745
taphead <techne.trax@proton.me>
2025-05-25 11:04:11 +0600

    Added form and localstorage functionality, updated css

--- app.js
+++ app.js
@@ -1,22 +1,76 @@
 const url = "https://jsonplaceholder.typicode.com/users";
 const cardContainer = document.querySelector(".container");
+let localStorageData = JSON.parse(localStorage.getItem("userList"));
 let userArray = [];
+
 async function fetchUsers(url) {
   const response = await fetch(url);
   const data = await response.json();
   userArray = [...data];
+  setLocalStorage();
 }
 
 async function initializeDataDisplay() {
   await fetchUsers(url);
   userArray.forEach((user) => {
-    const cardElement = document.createElement("div");
-    cardElement.className = "card";
-    cardElement.innerHTML =
-      user.name + "<br>" + user.phone + "<br>" + user.address.city;
-
-    cardContainer.appendChild(cardElement);
+    createCardElement(user);
   });
 }
 
-initializeDataDisplay();
+if (localStorageData) {
+  userArray = localStorageData;
+  localStorageData.forEach((user) => {
+    createCardElement(user);
+  });
+} else {
+  initializeDataDisplay();
+}
+
+document
+  .querySelector(".form")
+  .addEventListener("submit", (event) => handleFormSubmit(event));
+
+function handleFormSubmit(e) {
+  e.preventDefault();
+  const name = document.querySelector('input[name="name"]').value;
+  const phone = document.querySelector('input[name="phone"]').value;
+  const city = document.querySelector('input[name="city"]').value;
+  const website = document.querySelector('input[name="website"]').value;
+  e.target.reset();
+  newUser = {
+    name: name,
+    phone: phone,
+    address: { city: city },
+    website: website,
+  };
+  updateDataDisplay(newUser);
+}
+
+function updateDataDisplay(user) {
+  userArray.push(user);
+  createCardElement(user);
+  setLocalStorage();
+}
+
+function createCardElement(user) {
+  const cardElement = document.createElement("div");
+  cardElement.className = "card";
+  cardElement.innerHTML =
+    "Name: " +
+    user.name +
+    "<br>" +
+    "Phone Number: " +
+    user.phone +
+    "<br>" +
+    "City: " +
+    user.address.city +
+    "<br>" +
+    "Website: " +
+    user.website;
+
+  cardContainer.appendChild(cardElement);
+}
+
+function setLocalStorage() {
+  localStorage.setItem("userList", JSON.stringify(userArray));
+}
